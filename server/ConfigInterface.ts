5de687f23deda125faa7baa8825053f853e52ba2
Tankzorx <gustavj.dotzorx@gmail.com>
2017-12-05 21:09:33 +0100

    Adds ConfigInterface, so it's obvious how to configure the server.

---
+++ server/ConfigInterface.ts
@@ -0,0 +1,10 @@
+export default interface ConfigInterface {
+    hueInfo: {
+        bridgeIp: string,
+        username: string,
+    },
+    facebookDetails: {
+        email: string,
+        password: string;
+    }
+}
