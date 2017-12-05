371f212227105d53f9e06cc596080a3dc9edccf5
Tankzorx <gustavj.dotzorx@gmail.com>
2017-12-05 20:40:48 +0100

    Renamed react-backend to server, and removed bridge ip and username.

--- react-backend/config.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    bridgeIp: "192.168.0.17",
-    username: "uFU2e7sEn7I0aHr5zD00eurb8jrkscqOEgnYjeNI"
-}
---
+++ server/config.ts
@@ -0,0 +1,4 @@
+export default {
+    bridgeIp: "BRIDGE_IP",
+    username: "USER_ID"
+}
