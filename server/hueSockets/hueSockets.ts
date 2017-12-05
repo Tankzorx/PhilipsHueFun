b94b6991dd7f603d79138d65a013f1406d09c01e
Tankzorx <gustavj.dotzorx@gmail.com>
2017-12-05 21:13:10 +0100

    aaaaand actually use the new ConfigInterface correctly. durr.

--- server/hueSockets/hueSockets.ts
+++ server/hueSockets/hueSockets.ts
@@ -1,7 +1,7 @@
 import { HueApi, lightState } from "node-hue-api"
 
 
-import { hueInfo } from '../config'
+import config from '../config'
 export const server = require('http').createServer();
 // var io = require("socket.io")(server)
 import * as io from 'socket.io'
@@ -13,7 +13,7 @@ const ioServer = io(server)
  * Set up hue api and socket.io
  */
   
-const hueApi = new HueApi(hueInfo.bridgeIp, hueInfo.username);
+const hueApi = new HueApi(config.hueInfo.bridgeIp, config.hueInfo.username);
 
 export const initHueSockets = () => {
   hueApi.lights().then( (result) => {
--- server/routes/hueRoutes.ts
+++ server/routes/hueRoutes.ts
@@ -2,9 +2,9 @@ import { HueApi, lightState } from "node-hue-api"
 var express = require('express');
 var router = express.Router();
 
-import { hueInfo } from '../config'
+import config from '../config'
 
-const hueApi = new HueApi(hueInfo.bridgeIp, hueInfo.username);
+const hueApi = new HueApi(config.hueInfo.bridgeIp, config.hueInfo.username);
 
 router.get('/', function(req, res, next) {
   res.send("hueRoutes")
